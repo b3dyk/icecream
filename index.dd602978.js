d4afb7dea8df4350a38f24e94d62c795cc73d3e6
b3dyk <oleksandrperlov@gmail.com>
2022-08-12 08:43:09 +0000

    Deploying to gh-pages from @ b3dyk/team-project-icecream@bb21988ca12cb08d1863c3bc8775a08c4e2bf815 🚀

--- index.dd602978.js
+++
@@ -1,2 +0,0 @@
-$(".single-item").slick({dots:!0}),(()=>{const e={openMobileMenuBtn:document.querySelector("[data-mobile-menu-open]"),closeMobileMenuBtn:document.querySelector("[data-mobile-menu-close]"),mobileMenu:document.querySelector("[data-mobile-menu]")};function n(){e.mobileMenu.classList.toggle("is-hidden")}e.openMobileMenuBtn.addEventListener("click",n),e.closeMobileMenuBtn.addEventListener("click",n)})();
-//# sourceMappingURL=index.dd602978.js.map
--- index.ee5e04c0.js
+++
@@ -1,2 +0,0 @@
-$(".single-item").slick({dots:!0}),function(){var e={openMobileMenuBtn:document.querySelector("[data-mobile-menu-open]"),closeMobileMenuBtn:document.querySelector("[data-mobile-menu-close]"),mobileMenu:document.querySelector("[data-mobile-menu]")};function n(){e.mobileMenu.classList.toggle("is-hidden")}e.openMobileMenuBtn.addEventListener("click",n),e.closeMobileMenuBtn.addEventListener("click",n)}();
-//# sourceMappingURL=index.ee5e04c0.js.map
